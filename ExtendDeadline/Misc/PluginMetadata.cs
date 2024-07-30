683d76ba47b1cfbd146b2d76bca88b29b0d8ebfe
WhiteSpike <58040140+WhiteSpike@users.noreply.github.com>
2024-07-30 11:07:10 +0100

    Updated metadata

--- ExtendDeadline/Misc/PluginMetadata.cs
+++ ExtendDeadline/Misc/PluginMetadata.cs
@@ -2,7 +2,7 @@
 {
     static class Metadata
     {
-        public const string GUID = "WhiteSpike.ExtendDeadline";
+        public const string GUID = "com.github.WhiteSpike.ExtendDeadline";
         public const string NAME = "Extend Deadline";
         public const string VERSION = "1.0.0";
     }
